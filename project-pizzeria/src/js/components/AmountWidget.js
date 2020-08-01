fcceab0f6a7909702320e0e197977a3e3bb4536b
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-08-01 18:53:54 +0200

    Modify js-files in src - export, import

--- project-pizzeria/src/js/components/AmountWidget.js
+++ project-pizzeria/src/js/components/AmountWidget.js
@@ -1,60 +1,61 @@
-{
-  class AmountWidget{
-    constructor(element){
-      const thisWidget = this;
-      thisWidget.getElements(element);
-      thisWidget.initActions();
-      thisWidget.value = settings.amountWidget.defaultValue;
+import {settings, select} from './settings.js';
+
+class AmountWidget{
+  constructor(element){
+    const thisWidget = this;
+    thisWidget.getElements(element);
+    thisWidget.initActions();
+    thisWidget.value = settings.amountWidget.defaultValue;
+    thisWidget.setValue(thisWidget.input.value);
+    
+    //console.log('AmountWidget:', thisWidget);
+    //console.log('constructor arguments:', element);
+    //console.log('thisWidget.value', thisWidget.value);
+  }
+  //met. getElements to find elements of the widget
+  getElements(element){
+    const thisWidget = this;     
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+  }
+  //met. to set up new value of the widget
+  setValue(value){
+    const thisWidget = this;
+    const newValue = parseInt(value);
+    
+    /* TODO: Add validation */
+    if(newValue !=thisWidget.value && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax){
+      thisWidget.value = newValue;
+      thisWidget.announce();
+    }
+    thisWidget.input.value = thisWidget.value;
+  }
+  //met. initActions() - to add reaction to events
+  initActions(){
+    const thisWidget = this;
+    
+    thisWidget.input.addEventListener('change', function(){
       thisWidget.setValue(thisWidget.input.value);
-      
-      //console.log('AmountWidget:', thisWidget);
-      //console.log('constructor arguments:', element);
-      //console.log('thisWidget.value', thisWidget.value);
-    }
-    //met. getElements to find elements of the widget
-    getElements(element){
-      const thisWidget = this;     
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    }
-    //met. to set up new value of the widget
-    setValue(value){
-      const thisWidget = this;
-      const newValue = parseInt(value);
-      
-      /* TODO: Add validation */
-      if(newValue !=thisWidget.value && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax){
-        thisWidget.value = newValue;
-        thisWidget.announce();
-      }
-      thisWidget.input.value = thisWidget.value;
-    }
-    //met. initActions() - to add reaction to events
-    initActions(){
-      const thisWidget = this;
-      
-      thisWidget.input.addEventListener('change', function(){
-        thisWidget.setValue(thisWidget.input.value);
-      });
-      thisWidget.linkDecrease.addEventListener('click', function(event){
-        event.preventDefault();
-        thisWidget.setValue(thisWidget.value - 1);
-      });
-      thisWidget.linkIncrease.addEventListener('click', function(event){
-        event.preventDefault();
-        thisWidget.setValue(thisWidget.value + 1);
-      });
-    }
-    announce(){
-      const thisWidget = this;
-      //const event = new Event('updated'); CHANGE TO:
-      const event = new CustomEvent('updated', {
-        bubbles: true
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
+    });
+    thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+  announce(){
+    const thisWidget = this;
+    //const event = new Event('updated'); CHANGE TO:
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
   }
 }
+
 export default AmountWidget;
