d4a0c1b66cc7ed09d23d625db545cf4668311b2f
Aleksandra Joachimiak <joachim07@poczta.onet.pl>
2020-09-13 18:02:39 +0200

    Modify MainPage.js, slideShow()

--- project-pizzeria/src/js/components/MainPage.js
+++ project-pizzeria/src/js/components/MainPage.js
@@ -15,41 +15,30 @@ class MainPage {
   
   makeLinksActive(){  //make nav-link: nav-link-order & nav-link-booking active at the 'click' moment
     const thisMainPage = this;
-    thisMainPage.inMainPageLinks = document.querySelectorAll(select.inMainPageLinks); //to find all links
-    
+        
     const linkOrder = document.querySelector('.nav-link-order');
     const linkBooking = document.querySelector('.nav-link-booking');
-    
-    const linkOrderId = linkOrder.getAttribute('href').replace('#', ''); //href="#order"
-    const linkBookingId = linkBooking.getAttribute('href').replace('#', ''); //href="#booking"
-    
     const pages = document.querySelector(select.containerOf.pages).children; // all my pages
+    const links = [linkOrder, linkBooking];
     
-    //first case - click on linkOrder: .nav-link-order
-    linkOrder.addEventListener('click', function(){
-      for(let page of pages){
-        if(page.id === linkOrderId){
-          page.classList.add(classNames.pages.active);
-        } else {
-          page.classList.remove(classNames.pages.active);
+    for(let link of links){
+      link.addEventListener('click', function(){
+        const linkId = link.getAttribute('href').replace('#', '');
+        for(let page of pages){
+          if(page.id === linkId){
+            page.classList.add(classNames.pages.active);
+          } else {
+            page.classList.remove(classNames.pages.active);
+          }
         }
-      }
-    });
-    //second case - click on linkBooking: .nav-link-booking
-    linkBooking.addEventListener('click', function(){
-      for(let page of pages){
-        if(page.id === linkBookingId){
-          page.classList.add(classNames.pages.active);
-        } else {
-          page.classList.remove(classNames.pages.active);
-        }
-      }
-    });
+      });
+    }
   }
   
   getElements(){
     const thisMainPage = this; 
     thisMainPage.opinion = document.querySelector(select.containerOf.opinion);
+    thisMainPage.inMainPageLinks = document.querySelectorAll(select.inMainPageLinks); //to find all links
   }
   
   getData(){
@@ -64,42 +53,75 @@ class MainPage {
         console.log('parsedResponse', parsedResponse);
         thisMainPage.opinions = parsedResponse;
         thisMainPage.prepareOpinions();
-        return parsedResponse;
       });
-    
   }
   
   prepareOpinions(){
     const thisMainPage = this;
     console.log('thisMainPage.opinion', thisMainPage.opinions);
     const allOpinions = thisMainPage.opinions;
-     
-    for (let theCurentOpinion of allOpinions){ // one = curent opinion of the all opinions got from server
-      console.log('theCurentOpinion', theCurentOpinion); // show in console the all content of the object: theCurentOpinion
+    
+    //find all containers of the current opinion
+    const opinionElements = document.querySelectorAll('.opinion-element');
+    
+    let index = 0;
+    for(let currentOpinion of allOpinions){
       
-      /*
-      for (let theCurentOpinionItem in theCurentOpinion){
-        console.log('theCurentOpinionItem', theCurentOpinionItem); // show all kinds of items: id, title, text, author...
+      //prepare access to one of the opinion divs
+      const elem = opinionElements[index];
+      
+      //prepare shortcuts to theCurrentOpnion params
+      const { title, text, author } = currentOpinion;
+      
+      //find: 1.title element, 2.text element, 3.author element, in elem and set innerHTML to const: title, text, author.
+      const titleElem = elem.querySelector('.opinion-title');
+      titleElem.innerHTML = title;
+
+      const textOpinionElem = elem.querySelector('.opinion-text');
+      textOpinionElem.innerHTML = text;
+
+      const authorOpinionElem = elem.querySelector('.opinion-author');
+      authorOpinionElem.innerHTML = author;
+
+      index++;
+    }
+  }
+
+  makeCarousel(){
+    /* eslint-disable-next-line no-unused-vars */
+    const thisMainPage = this;
+    
+    //make slides show:
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides(){
+      const slides = document.querySelectorAll('.slides');
+      const dots = document.querySelectorAll('.dot');
+      
+      for(let slide of slides){
+        slide.style.display = 'none';  
       }
-      */
       
-      const id = theCurentOpinion.id;
-      console.log('id', id);
+      for(let dot of dots){
+        dot.classList.remove('active');
+      }
       
-      const title = theCurentOpinion.title;
-      console.log('title', title);
+      if (slideIndex === slides.length) slideIndex = 0; 
+      slides[slideIndex].style.display = 'block';  
+      dots[slideIndex].classList.add('active');
       
-      const text = theCurentOpinion.text;
-      console.log('text', text);
-      
-      const author = theCurentOpinion.author;
-      console.log('author', author);
-      
-      // a propper opinion, got from server put into opinion wrapper in index.html
-      const opinionElements = document.querySelectorAll('.opinion-element');
-      console.log('opinionElements', opinionElements);
-      
-      for (let opinionElement of opinionElements){
+      slideIndex++;
+      setTimeout(showSlides, 2500);
+    }
+  }
+  
+  
+}
+export default MainPage;
+/*
+/////////////////////////////////////////////
+    for (let opinionElement of opinionElements){
         
         //TITLE
         const titleOpinionElements = document.querySelectorAll('.opinion-title');
@@ -133,74 +155,4 @@ class MainPage {
           
         }
       }
-    }
-  }
-
-  makeCarousel(){
-    /* eslint-disable-next-line no-unused-vars */
-    const thisMainPage = this;
-    
-    //make slides show:
-    var slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-      var i;
-      const slides = document.getElementsByClassName('slides');
-      const dots = document.getElementsByClassName('dot');
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1;}    
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '');
-      }
-      slides[slideIndex-1].style.display = 'block';  
-      dots[slideIndex-1].className += ' active';
-      setTimeout(showSlides, 2500);
-    }
-  }
-  
-  
-}
-export default MainPage;
-/*
-/////////////////////////////////////////////
-    // opinion in index.html/wrapper
-      const opinionElements = document.querySelectorAll('.opinion-element');
-      console.log('opinionElements', opinionElements);
-      
-    for (let opinionElement of opinionElements){
-        const opinionAtributteNr = opinionElement.getAttribute('data');
-        const opinionElementNr = opinionAtributteNr.replace('wrapper-', '');
-        console.log('opinionElementNr', opinionElementNr);
-      }
-
-//const opinionNr = opinionWrapper.getAttribute('data');
-//const opinionDataNr = opinionNr.replace('wrapper-', '');
-//console.log('opinionDataNr', opinionDataNr);
-//if(titleDataNr === id){
-//const showTitle = title(id);
-//console.log('showTitle', showTitle);
-//}
-/*
-//const opinionTitle = opinionWrapper.title;
-          //opinionTitle.innerHTML = title;
-          //opinionWrapper.title = title;
-          
-          const opinionText = opinionWrapper.text;
-          opinionWrapper.text = text;
-          
-          const opinionAuthor = opinionWrapper.author;
-          opinionWrapper.author = author;
-          
-          //console.log('opinionWrapper.title', opinionWrapper.title);
-          //console.log('opinionWrapper.text', opinionWrapper.text);
-          
-          
-          for (let titleWrapperItem in titleWrapper){
-            const opinionTitle = titleWrapper.title;
-            console.log('opinionTitle', titleWrapperItem);
-          }
  */
